b74f251f784edbc3194025ba8b3165f7de2f2a3e
janath <janathhma@gmail.com>
2024-01-13 17:23:21 +0530

    create Reset account password user interface

---
+++ src/main/java/contoroller/LoginFormController.java
@@ -0,0 +1,7 @@
+package contoroller;
+
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public AnchorPane paneid;
+}
---
+++ src/main/java/contoroller/passwordResetFormController.java
@@ -0,0 +1,4 @@
+package contoroller;
+
+public class passwordResetFormController {
+}
