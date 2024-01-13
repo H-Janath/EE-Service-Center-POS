1299e8c9ac6d8e38c775918a8b69d11b3356a4e6
janath <janathhma@gmail.com>
2024-01-13 10:31:04 +0530

    Add Appinitializer class for JavaFX application initialization

---
+++ src/main/java/Appinitializer.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Appinitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("./view/loginForm.fxml"))));
+            primaryStage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
